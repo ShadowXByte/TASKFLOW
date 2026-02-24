80a98e19f58afd7f1416fc362a71ae4492ad74f8
SIAM HOSSAIN DHALI <96874558+ShadowXByte@users.noreply.github.com>
2026-02-24 14:05:48 +0600

    .

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,6 +11,16 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#0f172a",
     icons: [
       {
+        src: "/unnamed.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/unnamed.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
         src: "/unnamed.jpg",
         sizes: "192x192",
         type: "image/jpeg",
